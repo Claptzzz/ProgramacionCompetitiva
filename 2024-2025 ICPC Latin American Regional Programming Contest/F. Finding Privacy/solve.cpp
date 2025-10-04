5fbe6c6760739ddc579afebcca1602cbfb1632df
Nicolás Rojas Bustos <nicolas.rojas13@hotmail.com>
2025-10-04 01:09:50 -0300

    first try ej F icpc 2024 latin america

--- 2024-2025 ICPC Latin American Regional Programming Contest/F. Finding Privacy/solve.cpp
+++ 2024-2025 ICPC Latin American Regional Programming Contest/F. Finding Privacy/solve.cpp
@@ -5,7 +5,41 @@ using namespace std;
 
 int main() {
 
+    int k,n; cin>>k>>n;
 
+    if (n-k < 2) {
+        cout << "*";
+        return 0;
+    } else if (n-k > 3) {
+        cout << "*";
+        return 0;
+    } else {
+        bool max = true;
+        int x = n;
+        while (max) {
+            cout << "X"; 
+            k--; 
+            x--; 
+            if (k == 0) break;
+            if (x / k <= 2) {
+                !max;
+                break;
+            }
+            if (x > 1) {
+                cout << "--";
+                x -= 2;
+            } else if (x == 1) {
+                cout << "-";
+                x--;
+            }
+        }
+        if (x > 0) cout << "-"; x--;
+        while (x > 0) {
+            cout << "X";
+            x--;
+            if (x>0) cout << "-"; x--;
+        }
+    }
 
     return 0;
 }
