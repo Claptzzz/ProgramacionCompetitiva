972a3f4f4daf96e04a2df583766773e08528c3ec
Nicolás Rojas Bustos <nicolas.rojas13@hotmail.com>
2025-11-03 16:37:32 -0300

    add solve f icpc 2024 f de facil

--- 2024-2025 ICPC Latin American Regional Programming Contest/F. Finding Privacy/solve.cpp
+++ 2024-2025 ICPC Latin American Regional Programming Contest/F. Finding Privacy/solve.cpp
@@ -7,17 +7,47 @@ int main() {
 
     int k,n; cin>>k>>n;
 
-    difMax = 1+k*2;
-
-    if (n-k < 2) {
+    int difMax = 1+k*2;
+    int difMin = k-1;
+    int dif = n-k;
+    if (dif < 0) {
+        dif *= -1;
+    }
+    if (dif < difMin) {
         cout << "*";
         return 0;
-    } else if (n-k >= difMax) {
+    } else if (dif >= difMax) {
         cout << "*";
         return 0;
     } else {
-        int guiones = n-k;
-        
+        if (difMax-dif == 1) {
+            cout << "-";
+            n--;
+            while (n != 2) {
+                cout << "X--";
+                n -= 3; 
+            }
+            cout << "X-";
+        } else {
+            
+            while (dif > difMin && n>3) {
+                cout << "X" << "--";
+                dif--;
+                k--;
+                n -= 3;
+            }
+            while (n > 0 ) {
+                if (n>0 && k>0) {
+                    cout << "X";
+                    k--;
+                    n--;
+                }
+                if (n>0) {
+                    cout << "-";
+                    n--;
+                }
+            } 
+        }
     }
 
     return 0;
