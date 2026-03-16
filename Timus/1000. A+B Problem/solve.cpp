7abc7b5adb5db20a216007cf68658babe583a1ba
Claptzzz <nicolas.rojas13@hotmail.com>
2026-03-15 23:06:35 -0300

    minor changes

--- Timus/1000. A+B Problem/solve.cpp
+++ Timus/1000. A+B Problem/solve.cpp
@@ -1,12 +1,12 @@
-//https://acm.timus.ru/problem.aspx?space=1&num=1000
+// https://acm.timus.ru/problem.aspx?space=1&num=1000
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
 
-    int a,b;cin>>a>>b;
-    cout << a+b << endl;
-
-    return 0;
+  int a, b;
+  cin >> a >> b;
+  cout << a + b;
+  return 0;
 }
